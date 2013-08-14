c04d123f77aa29e3432986181b3268d257416366
Joel Holdbrooks <cjholdbrooks@gmail.com>
2013-08-13 18:20:57 -0700

    Begin work on command line version (cljs)

--- project.clj
+++ project.clj
@@ -3,5 +3,23 @@
   :url "http://github.com/noprompt/frak"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]]
-  :profiles {:dev {:dependencies [[criterium "0.4.1"]]}} )
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :plugins [[lein-cljsbuild "0.3.2"]]
+  :source-paths ["src/clj"]
+  :profiles {:dev {:dependencies [[criterium "0.4.1"]]}}
+  :cljsbuild {:crossovers [frak]
+              :crossover-path "crossovers"
+              :crossover-jar true
+              :builds [{:id "dev"
+                        :source-paths ["src/clj" "src/cljs"]
+                        :compiler {:output-to "bin/frak.dev.js"
+                                   :optimizations :simple
+                                   :pretty-print true
+                                   :target :nodejs}}
+                        
+                       {:id "prod"
+                        :source-paths ["src/clj" "src/cljs"]
+                        :compiler {:output-to "bin/frak.prod.js"
+                                   :optimizations :simple
+                                   :pretty-print false
+                                   :target :nodejs}}]})
